757a845700efe7c6ecc66ffff91cbb398d1b0bdf
Tan Yan Lyn <e0424594@u.nus.edu>
2021-05-20 17:42:33 +0800

    Removed unused code

--- src/main/java/OlderCode/Publisher.java
+++
@@ -1,5 +0,0 @@
-package OlderCode;
-
-public interface Publisher {
-    void publish(Event message, PubSubBroker broker);
-}
--- src/main/java/OlderCode/PublisherConc.java
+++
@@ -1,7 +0,0 @@
-package OlderCode;
-
-public class PublisherConc implements Publisher {
-    public void publish(Event message, PubSubBroker pubSubBroker) {
-        pubSubBroker.addMessage(message);
-    }
-}
